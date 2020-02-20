9161173aaedfa98114c101bcbb4745fd1935d245
Itsarapong Rattanasak <savezts2@gmail.com>
2020-02-20 08:26:57 +0700

    safeis

--- BookMeetingRoom/src/app/app.component.ts
+++ BookMeetingRoom/src/app/app.component.ts
@@ -5,8 +5,8 @@ import { Observable } from "rxjs";
 import { AuthService } from './auth.service';
 
 //export const baseUrl = 'http://172.27.209.27:8080/BookMeetingRoom';
-//export const baseUrl = 'http://localhost:8081';
-export const baseUrl = 'http://192.168.1.40:8081';
+export const baseUrl = 'http://localhost:8081';
+//export const baseUrl = 'http://192.168.1.40:8081';
 
 @Component({
   selector: 'app-root',
